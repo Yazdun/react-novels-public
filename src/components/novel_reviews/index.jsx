304f3850ccb79c5832e32380728e820c960d14ba
Yazdun <yazduntube@gmail.com>
2022-02-03 11:31:08 +0330

    novel reviews : added no reviews text

--- src/components/novel_reviews/index.jsx
+++ src/components/novel_reviews/index.jsx
@@ -1,12 +1,12 @@
 import s from "./styles.module.scss";
 import classnames from "classnames";
-import { RenderText, Text } from "../../elements";
+import { Loading, RenderText, Text } from "../../elements";
 import { useGet } from "../../hooks";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { GET_NOVEL_REVIEWS } from "../../services";
-import { placeholder } from "../../assets";
 import { Rate, UserCard } from "..";
+
 export const NovelReviews = () => {
   const { getRequest, getLoading } = useGet();
   const { id } = useParams();
@@ -17,6 +17,20 @@ export const NovelReviews = () => {
   useEffect(() => {
     getRequest(GET_NOVEL_REVIEWS(id), handleReviews);
   }, [id]);
+
+  if (getLoading) {
+    return <Loading height={300} />;
+  }
+
+  if (reviews.length < 1) {
+    return (
+      <div className={s.reviews}>
+        <Text bold customclass={s.text}>
+          Be the first one to review this novel 👏
+        </Text>
+      </div>
+    );
+  }
   return (
     <div className={s.reviews}>
       <Text bold customclass={s.text}>
