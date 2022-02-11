8123e849973aff398efe75c608f64c231c265b5c
Yazdun <yazduntube@gmail.com>
2022-02-11 20:09:32 +0330

    context : edited

--- src/App.js
+++ src/App.js
@@ -4,25 +4,31 @@ import { AppRoutes, PrivateRoute } from "./routes";
 import { Route, Switch } from "react-router-dom";
 import { Alert, Navigation } from "./components";
 import { BackToTop } from "./components/";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context";
 
 function App() {
   return (
-    <ErrorHandler>
-      <NoticesProvider>
-        <Navigation />
-        <Alert />
-        <BackToTop />
-        <Switch>
-          {AppRoutes.map((route, item) =>
-            route.private ? (
-              <PrivateRoute key={item} {...route} />
-            ) : (
-              <Route key={item} {...route} />
-            )
-          )}
-        </Switch>
-      </NoticesProvider>
-    </ErrorHandler>
+    <BrowserRouter basename="/index.html">
+      <AuthProvider>
+        <ErrorHandler>
+          <NoticesProvider>
+            <Navigation />
+            <Alert />
+            <BackToTop />
+            <Switch>
+              {AppRoutes.map((route, item) =>
+                route.private ? (
+                  <PrivateRoute key={item} {...route} />
+                ) : (
+                  <Route key={item} {...route} />
+                )
+              )}
+            </Switch>
+          </NoticesProvider>
+        </ErrorHandler>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
 
 TimeAgo.addDefaultLocale(en);
 
-ReactDOM.render(
-  <BrowserRouter basename="/index.html">
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
